798fbf12fb2c945d051b10a8256ac8a320051b0b
hansen9 <79235117+hansen9@users.noreply.github.com>
2022-11-06 21:41:04 +0700

    Player running anim finished

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     private Animator myAnimator;
 
+    private bool facingRight = true;
     //variables to play with
     public float speed = 2.0f;
     public float horizMovement;// 1[OR]-1[OR]0
@@ -33,5 +34,19 @@ public class PlayerMovement : MonoBehaviour
     {
         //move the character left and right
         rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+        Flip(horizMovement);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+    }
+
+    //flipping function
+    private void Flip(float horizontal)
+    {
+        if(horizontal < 0 && facingRight || horizontal > 0 && !facingRight){
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
